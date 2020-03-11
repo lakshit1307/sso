99b35ee1ae1dc875c49de4912e9e3ac03f490193
lpande <lpande@healthedge.com>
2020-03-11 07:50:14 -0400

    Added token in the login response

--- src/main/java/com/healthedge/sso/dto/LoginResponse.java
+++ src/main/java/com/healthedge/sso/dto/LoginResponse.java
@@ -2,10 +2,12 @@ package com.healthedge.sso.dto;
 
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class LoginResponse extends BaseResponse {
 
     private String token;
